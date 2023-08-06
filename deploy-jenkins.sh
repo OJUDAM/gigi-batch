c300aa7d7aeccb3808eac0a0c45c5084c6a6984b
OJUDAM <dhwnkea@naver.com>
2023-08-06 21:20:47 +0900

    젠킨스 테스트2

--- deploy-jenkins.sh
+++ deploy-jenkins.sh
@@ -7,14 +7,14 @@ then
 elif [ $1 != "dev" ] && [ $1 != "prod" ]; then
     echo ">> 첫번째 인자 값은 dev(개발) or prod(운영)로 입력해주세요."
 else
-    cd /var/lib/jenkins/workspace/gigi
+    cd /var/lib/jenkins/workspace/gigi-batch
 
     sudo ./gradlew bootJar
 
     cd ./build/libs
 
-    sudo scp -i ../../../key/gigi.pem gigi.war ubuntu@158.180.68.72:/home/ubuntu/gigi/batch
-    sudo ssh -i ../../../key/gigi.pem -t ubuntu@158.180.68.72 -T "cd /home/ubuntu/gigi/batch ; bash;" "sudo ./gigi-start.sh gigiScheduler.jar $1"
+    sudo scp -i /var/lib/jenkins/key/gigi.pem gigiScheduler.jar ubuntu@158.180.68.72:/home/ubuntu/gigi/batch
+    sudo ssh -i /var/lib/jenkins/key/gigi.pem -t ubuntu@158.180.68.72 -T "cd /home/ubuntu/gigi/batch ; bash;" "sudo ./gigi-start.sh gigiScheduler.jar $1"
 
     cd ../..
 fi
